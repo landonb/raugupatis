a57cd4bb6a9b69be1d17aecac3f89c13f8765d18
Landon Bouma <landon.bouma@gmail.com>
2016-11-04 01:13:07 -0500

    Still commenting.

--- hella-ps-ino/hella-ps-ino.ino
+++ hella-ps-ino/hella-ps-ino.ino
@@ -1,4 +1,4 @@
-// Last Modified: 2016.11.03
+// Last Modified: 2016.11.04
 // Project Page: https://github.com/landonb/raugupatis
 // Description: Ardruinko Schketch*hic*.
 // vim:tw=0:ts=4:sw=4:noet:
@@ -7,16 +7,17 @@
 
 // This file is just a shim.
 //
-// - [lb] is using the Arduino IDE and I find myself copy-pasting
-//   this file to the IDE every time I edit it, so I moved all the
-//   code to header files and will just edit those (which I find
-//   that I don't have to copy-paste to the IDE).
+// - 2016-11-03: [lb]: This is my first sketch and first time using
+//   the Arduino IDE, and I find myself copy-pasting this file to the
+//   IDE every time I edit it, so I moved all the code to other files
+//   and just edit those.
 //
-//   Also, after the "Upload" action, the IDE also rewrites this
+//   Also, after the "Upload" action, the IDE rewrites this
 //   file and reformats it with 2-spaced indenting. Crazy IDE.
 //
-//   Oh, nm, I guess via File > Preferences you can also just
-//   uncheck the "Save when verifying or uploading" checkbox.
+//     Unless you File > Preferences and uncheck
+//      "Save when verifying or uploading"
+//     but still.
 
 // Built-in Arduino one-time setup routine.
 void setup()
