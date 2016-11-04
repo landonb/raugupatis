cc3a70e873515ad7bededee07ef5a7ee736b63e0
Landon Bouma <landon.bouma@gmail.com>
2016-11-03 19:20:26 -0500

    New module for talking to Pi.

---
+++ hella-ps-ino/comm.cpp
@@ -0,0 +1,17 @@
+// Last Modified: 2016.11.03
+// Project Page: https://github.com/landonb/raugupatis
+// Description: Ardruinko Schketch*hic*.
+// vim:tw=0:ts=4:sw=4:noet:
+
+#include "Arduino.h"
+
+#include "comm.h"
+
+boolean comm_authenticate(int rfid_tag[4]) {
+	boolean authenticated = false;
+
+	// FIXME: Until this is wired....
+	authenticated = true;
+	return authenticated;
+}
+
---
+++ hella-ps-ino/comm.h
@@ -0,0 +1,9 @@
+// Last Modified: 2016.11.03
+// Project Page: https://github.com/landonb/raugupatis
+// Description: Ardruinko Schketch*hic*.
+// vim:tw=0:ts=4:sw=4:noet:
+
+boolean comm_authenticate(int rfid_tag[4]);
+
+void comm_update_state(int rfid_tag[4]);
+
